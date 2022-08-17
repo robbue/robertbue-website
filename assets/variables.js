ec3fdff91d2d9d7bae0266b77e07c84fcb1c9faf
Robert Bue <rb@trypearl.no>
2022-08-18 00:53:20 +0200

    Set color dark and theme color as same

--- assets/variables.js
+++ assets/variables.js
@@ -22,7 +22,7 @@ const variables = {
 	'color-primary': '#bd9b60', // ffcc66 // ff8763 // fffd7f // ffc87f // e0caca // ffecb5
 	'color-accent': '#fff334', // ffc87f
 	'color-secondary': 'pink',
-	'color-dark': '#0f2d3e', // 0a0a0a
+	'color-dark': '#112b3a', // 0a0a0a
 	'color-darkgrey': '#444444',
 	'color-grey': '#94b2c5', // 696969
 	'color-white': '#fff',
